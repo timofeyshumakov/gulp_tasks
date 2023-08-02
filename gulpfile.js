d7185437a2e5a2cf8f4823f60756c54ee9101c77
timofeyshumakov <136724219+timofeyshumakov@users.noreply.github.com>
2023-08-02 14:13:09 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -9,23 +9,22 @@ var rigger = require('gulp-rigger');
 var uglify = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
 var pug = require('gulp-pug');
+var svgmin =require('gulp-svgmin');
 
 // Save a reference to the `reload` method
 
 // Watch scss AND html files, doing different things with each.
-gulp.task('serve', function () {
+gulp.task('serve', (cb) =>{
 
-    // Serve files from the root of this project
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-
-    gulp.watch("*.html").on("change", reload);
-
+ gulp.src('projtect_tmplate/source/*.svg')
+    .pipe(svgmin())
+    .pipe(gulp.dest('dist/'));
+	cb();
 });
+
+
 exports.watchers = () => {
+	 
 	    browserSync.init({
         server: {
             baseDir: "./"
@@ -60,21 +59,11 @@ exports.watchers = () => {
   });
 };
 
+exports.defaultTask = (cb) => {
+  gulp.src('../sourse/*.svg')
+  
+    .pipe(svgmin())
+ .pipe(gulp.dest('../img'))
+	cb();
+};
 
-exports.default = () => (
-	gulp.src('css\\style.css')
-		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-		.pipe(cssbeautify())
-		.pipe(stripCssComments())
-		.pipe(gulp.dest('dist/'))
-		.pipe(browserSync.reload({stream: true})) 
-);
-gulp.task('jst', async ()=> {
-    return gulp.src(['js/*.js']) 
-        .pipe(rigger())
-		.pipe(uglify())
-        .pipe(gulp.dest('dist/'));
-});
-
-exports.jst = 'jst';
-exports.sass = 'sass';
